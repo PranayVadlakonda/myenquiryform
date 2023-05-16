2447160f4b0a2b9674995ad6efd2865d49712b60
pranay <pranayanand61@gmail.com>
2023-05-16 21:06:09 +0530

    added main

---
+++ src/main/java/com/niit/Form.java
@@ -0,0 +1,10 @@
+package com.niit;
+
+public class Form {
+    public static void main(String[] args) {
+        double annualSalary = 10000;
+        System.out.println("annualSalary = " +"$"+ annualSalary);
+
+    }
+}
+
--- src/main/java/com/niit/jap/Main.java
+++
@@ -1,7 +0,0 @@
-package com.niit.jap;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
