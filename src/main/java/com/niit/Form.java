b4a1b807d5f7a3b5c2c270cba7e632fff5b9e515
pranay <pranayanand61@gmail.com>
2023-05-16 21:27:56 +0530

    declared and initialised the variable monthly salary

--- src/main/java/com/niit/Form.java
+++ src/main/java/com/niit/Form.java
@@ -2,8 +2,8 @@ package com.niit;
 
 public class Form {
     public static void main(String[] args) {
-        double annualSalary = 10000;
-        System.out.println("annualSalary = " +"$"+ annualSalary);
+        int monthlySalary = 2500;
+        System.out.println(monthlySalary);
 
     }
 }
